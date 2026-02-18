59bc029b99db543d54b82c2141e85292a063ce8b
Maksim Alimpiev <maks2005127@gmail.com>
2026-02-18 17:35:28 +0300

    vite

--- script/menu.js
+++ script/menu.js
@@ -15,7 +15,7 @@ export class Menu {
   async loadData() {
     let jsonData;
     try {
-      const response = await fetch('data.json');
+      const response = await fetch('/data.json');
       jsonData = await response.json();
     } catch (error) {
       console.error(`Не удается прочитать data.json\n\n${error}`);
