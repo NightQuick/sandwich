437a1810daa1a2f0dcb68808d7b106429b24d8ce
Maksim Alimpiev <maks2005127@gmail.com>
2026-03-16 09:54:25 +0300

    error on eng

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ export async function loadJson() {
     const response = await fetch('/data.json');
     jsonData = await response.json();
   } catch (error) {
-    console.error(`Не удается прочитать data.json\n\n${error}`);
+    console.error(`Error:can't connect to data\n\n${error}`);
   }
   return jsonData;
 }
