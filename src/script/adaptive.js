72b3eba6f0f35325eeca5afeaad4f728251eb55e
Maksim Alimpiev <maks2005127@gmail.com>
2026-03-25 10:19:49 +0300

    adaptive

---
+++ src/script/adaptive.js
@@ -0,0 +1,34 @@
+export function setWidth() {
+  window.addEventListener('resize', () => {
+    if (window.innerWidth <= 768) {
+      basketButton();
+    } else removeBasketButton();
+  });
+  const event = new Event('resize');
+  window.dispatchEvent(event);
+}
+function basketButton() {
+  const basketButton = document.createElement('button');
+  basketButton.id = 'basket-button';
+  const img = document.createElement('div');
+  img.id = 'basket-button-img';
+  basketButton.appendChild(img);
+
+  let basketOpened = false;
+  basketButton.addEventListener('click', () => {
+    const basket = document.getElementById('basket');
+    if (basketOpened) {
+      basket.classList.remove('basket-active');
+      basketOpened = false;
+    } else {
+      basket.classList.add('basket-active');
+      basketOpened = true;
+    }
+  });
+  document.getElementById('active-panel').appendChild(basketButton);
+}
+function removeBasketButton() {
+  if (document.getElementById('basket-button')) {
+    document.getElementById('basket-button').remove();
+  }
+}
--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,9 @@
 import { renderSwitcherTable } from '@script/table.js';
 import { subscribe } from '@script/subscribes.js';
 import { renderIngredientSwitcher } from '@script/ingredientSwitcher.js';
+import { setWidth } from '@script/adaptive.js';
+
+setWidth();
 
 renderSwitcherTable();
 renderIngredientSwitcher();
